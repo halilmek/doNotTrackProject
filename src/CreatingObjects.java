208402991df1d994b1c653db0b506d4a96745349
halilozturk <ztrkhll58@gmail.com>
2022-05-08 23:28:05 +0200

    everything changed

---
+++ src/CreatingObjects.java
@@ -0,0 +1,9 @@
+public class CreatingObjects {
+
+    public static void main(String[] args) {
+
+        WoodenSpoon wS1 = new WoodenSpoon("cemal",21,'m', false);
+
+        System.out.println(wS1);
+    }
+}
--- src/WoodenSpoon.java
+++ src/WoodenSpoon.java
@@ -10,8 +10,8 @@ public class WoodenSpoon {
 
         setName(name);
         setAge(age);
-        this.gender = gender;
-        this.isEligible = isEligible;
+        setGender(gender);
+        setEligible(age);
     }
 
 public void setName(String name) {
@@ -37,7 +37,7 @@ public void setAge(int age) {
         String age1 = age +"";
     for (int i = 0; i < age1.length(); i++) {
 
-        if (!Character.isDigit(age1.charAt(i)))
+        if (!Character.isDigit(age1.charAt(i)) || age <= 17)
         {
             throw new RuntimeException("age contains characters instead of number !!");
         }
@@ -54,17 +54,38 @@ public int getAge() {
 public void setGender(char gender) {
 
         String gender1 = gender + "";
-        if (!gender1.equalsIgnoreCase("m") || !gender1.equalsIgnoreCase("f")) {
+        //if (!gender1.equalsIgnoreCase("m") || !gender1.equalsIgnoreCase("f"))
+        if (gender == 'f' || gender == 'm'){
 
+            this.gender = gender;
+        }
+        else{
             throw new RuntimeException("another gender is not support !!!");
         }
-
-        this.gender = gender;
 }
 
 public char getGender() {
 
         return gender;
+}
+
+public void setEligible(int age) {
+
+        if (age > 17) {
+            isEligible = true;
+        }
+}
+public boolean getEligible() {
+
+        return isEligible;
+}
+
+public String toString() {
+
+        return "WoodenSpoon{ name = "+ name+
+                "\nage = "+ age+
+                "\ngender = "+ gender+
+                "\nisEligible = "+ isEligible;
 }
 
 }
